187e5e05561a3d72b7adff135ff960fe356915de
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2025-06-11 12:57:32 +0000

    OK

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/test")
+	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Teste Unitário está FUNCIONANDO!";
 	}
 
 }
